566a4fbbbb7082aec34abe4fc3c3abcf3687c989
Lokesh Kaushik <119752700+l-kaushik@users.noreply.github.com>
2022-12-23 21:39:50 +0530

    Adding turbo C supported file

---
+++ Guess_Turbo.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+	int i = 0, com, input, turn = 0, check = 1;
+    clrscr();
+	srand(time(NULL));
+	com = (int)rand() % 100;
+	while (i != 1)
+	{
+		turn++;
+		printf("press 1234 to exit\nGuess the computer's number:- ");
+		check = scanf("%d", &input);
+		getchar();
+		printf("\n");
+
+		if (check == 0)
+		{
+			printf("Please make sure you enter a number\n");
+
+		}
+		else if (input == 1234)
+		{
+			printf("Thanks for playing\n");
+			exit(0);
+		}
+		else if (input == com)
+		{
+			printf("You guessd it right in %d turn\n", turn);
+			break;
+		}
+		else
+		{
+			printf("\nOops, you guessed it wrong\n");
+			if (input > com)
+			{
+				printf("try a smaller number\n");
+			}
+			else
+			{
+				printf("try a greater number\n");
+			}
+		}
+		printf("\n");
+	}
+    getch();
+	return 0;
+}
